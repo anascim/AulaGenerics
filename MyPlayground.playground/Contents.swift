859fd7bb0b029e251d608b0cc037e3edd4bb4be8
anascim <alexjnascim@gmail.com>
2020-04-27 17:50:19 -0300

    exercicio juliao

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,49 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+func arrayContains<T: Hashable>(array: Array<T>, element: T) -> Bool {
+    for i in array {
+        if element == i {
+            return true
+        }
+    }
+    return false
+}
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+extension Array where Element: Numeric {
+    
+    func sumOfAll() -> Element {
+        var sum =  Element.zero
+        for e in self {
+            sum += e
+        }
+        return sum
+    }
+}
+
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
     //BONUS TODO: Fazer o método acima receber N arrays
+func commonElements<T: Equatable>(arrays: Array<T>...) -> [T]? {
+    var copy = arrays
+    let firstArray = copy.removeFirst()
+    var result = [T]()
+    
+    for element in firstArray {
+        for array in copy {
+            if !array.contains(element) {
+                break
+            }
+            if array == arrays.last {
+                result.append(element)
+            }
+        }
+    }
+    return result
+}
+
+let array1 = [0,0,2, 4, 7, 8, 9, 1, 456, 7345, 63]
+let array2 = [0,4, 8, 12, 654, 36, 54]
+let array3 = [0,5324, 2345, 13245, 412, 8, 342, 324, 643, 4]
+
+commonElements(arrays: array1, array2, array3)
